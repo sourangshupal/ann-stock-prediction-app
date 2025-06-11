aa5ca22b6e466f6cf7225c6308984ec58d287998
Sourangshu Pal <paulbindass@gmail.com>
2025-06-11 10:45:42 +0530

    code changes azure webapp push

--- fastapi_app.py
+++ fastapi_app.py
@@ -336,7 +336,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "fastapi_app:app",
         host="0.0.0.0",
-        port=8080,
+        port=8000,
         reload=True,
         log_level="info"
     )
